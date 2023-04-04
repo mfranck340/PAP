246f6fdb6bf2592acdaa40d38851eb830a177104
mfranck340 <franck.yaguargos@edu.uah.es>
2023-04-04 13:57:45 +0200

    Primer acercamiento con scala

--- Practica2_SCALA/PECL2/src/Main.scala
+++ Practica2_SCALA/PECL2/src/Main.scala
@@ -1,5 +1,40 @@
 object Main {
-  def main(args: Array[String]): Unit = {
-    println("Hello world!")
+
+  def concatenarListas(x: List[Int], y: List[Int]): List[Int] = {
+    x match {
+      case Nil => y
+      case n :: Nil => n :: y
+      case head :: tail => head :: concatenarListas(tail, y)
+    }
+  }
+
+  def tamLista(x:List[Int]): Int = {
+    x match {
+      case Nil => 0
+      case _ => 1 + tamLista(x.tail)
+    }
+  }
+
+  def main(args: Array[String]) {
+
+    println("\n- CUNDY CROSH SOGA -\n")
+    /*val rand = new scala.util.Random
+    val a = rand.nextInt(10)
+    println(a)
+    println(a)
+    val b = rand.between(1, 4)
+    println(b)*/
+
+    val tablero = new Tablero(5, 5, 6)
+    println(tablero.getContenido())
+
+    val x = List(1, 2, 3, 4)
+    val y = List(5, 6, 7, 8)
+    val z = concatenarListas(x, y)
+
+    print("Mostrar tablero inicial")
+    tablero.mostrarTablero()
+    //z.
+    //println(z)
   }
 }
---
+++ Practica2_SCALA/PECL2/src/Tablero.scala
@@ -0,0 +1,52 @@
+class Tablero(col:Int, fil:Int, dif:Int) {
+  private val N:Int = col
+  private val M:Int = fil
+  private val D:Int = dif
+  private val contenido:List[Int] = inicializarMatriz(N * M)
+
+  private def inicializarMatriz(celdas:Int): List[Int] = {
+     celdas match {
+       case 0 => Nil
+       case _ => 0 :: inicializarMatriz(celdas - 1)
+     }
+  }
+
+  private def concatenarListas(x:List[Int], y:List[Int]): List[Int] = {
+    x match {
+      case Nil => y
+      case n :: Nil => n :: y
+      case head :: tail => head::concatenarListas(tail, y)
+    }
+  }
+
+  private def mostrarTableroAux(x:List[Int]): Unit = {
+    x match {
+      case Nil =>  print("\n" + "---" * (N - 1) + "----")
+      case _ =>
+        if (tamLista(x) % N == 0) {
+          print("\n" + "---" * (N - 1) + "----" + "\n|")
+        }
+        if (x.head < 10)
+          print(x.head + " |")
+        else
+          print(x.head + "|")
+        mostrarTableroAux(x.tail)
+    }
+  }
+
+  private def tamLista(x:List[Int]): Int = {
+    x match {
+      case Nil => 0
+      case _ => 1 + tamLista(x.tail)
+    }
+  }
+
+  def mostrarTablero(): Unit = {
+    mostrarTableroAux(contenido)
+  }
+
+  def getContenido(): List[Int] = {
+    contenido
+  }
+}
+
