37a187ed01bd5b2f20f55bbad21da408efcedc4a
mfranck340 <franck.yaguargos@edu.uah.es>
2023-05-06 15:13:12 +0200

    fallo en la fecha

--- Practica3_CLOUD/PECL3-CLOUD/src/Game.scala
+++ Practica3_CLOUD/PECL3-CLOUD/src/Game.scala
@@ -48,7 +48,7 @@ class Game(args: List[String]) {
     import scalaj.http._
 
     val response = Http("http://express2458515403.azurewebsites.net/puntuacion")
-      .postData(s"{\"nombre\":\"$name\",\"puntos\":\"$puntuacion\",\"duracion\":\"$time\",\"fecha\":\"'${LocalDate.now()} ${format1.format(LocalTime.now())}'\"}")
+      .postData(s"{\"nombre\":\"$name\",\"puntos\":\"$puntuacion\",\"duracion\":\"$time\",\"fecha\":\"${LocalDate.now()} ${format1.format(LocalTime.now())}\"}")
       .header("content-type", "application/json")
       .asString
     println(response)
